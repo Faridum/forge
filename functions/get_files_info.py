0367c79b14b36454dde0f33b9de9a4b9fb330b83
Faridum <farid.jamaleldin@gmail.com>
2026-08-03 19:03:10 -0400

    feat: implement get_file_content function

---
+++ functions/get_files_info.py
@@ -0,0 +1,39 @@
+import os 
+
+def get_files_info(working_directory: str , directory: str = ".") -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+        valid_target_dir = (os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs)
+
+        if not valid_target_dir:
+            return(
+                f'Error: Cannot list "{directory}" '
+                "as it is outside the permitted working directory"
+            )
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        list_dir = os.listdir(target_dir)
+        results = []
+        
+
+        for item in list_dir:
+            full_path = os.path.join(target_dir, item)
+            file_size = os.path.getsize(full_path)
+            is_dir = os.path.isdir(full_path)
+
+            line = f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
+
+            results.append(line)
+
+
+
+        return "\n".join(results)
+        # new_target_dir = os.path.getsize(os.path.isdir(os.path.join(target_dir, item)))
+
+    except Exception as e:
+        return f"Error: {e}"
