040dcf7b7a912b4d5a0ea934d2c8f2fb2b52e498
Faridum <farid.jamaleldin@gmail.com>
2026-08-10 12:17:39 -0400

    feat: Add System Prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import argparse
+from prompts import system_prompt 
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
@@ -30,6 +31,11 @@ args = parser.parse_args()
 
 messages = [
     {
+        "role": "system",
+        "content": system_prompt
+    },
+
+    {
         "role": "user",
         "content": args.user_prompt
     },
@@ -38,6 +44,7 @@ messages = [
 response = client.chat.completions.create(
     model= "openrouter/free",
     messages=messages,
+    temperature=0,
 )
 
 if response.usage is None:
